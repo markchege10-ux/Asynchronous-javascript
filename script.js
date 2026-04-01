4f587d6496eaeed6f4eb5ceb23b88d5e6bafd756
mark muraya <markchege10@gmail.com>
2026-04-01 20:02:00 +0300

    changed some few things

--- script.js
+++ script.js
@@ -4,47 +4,39 @@ const loading = document.getElementById('loading');
 const error = document.getElementById('error');
 const weather = document.getElementById('weather');
 
-const API_KEY = '2e1c702e69d2cd1f7f5ee75c07081b63'
+const API_KEY = '2e1c702e69d2cd1f7f5ee75c07081b63';
 const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
     const city = input.value.trim();
     
-    if(!city) return;
-    
+    if (!city) return;
     await fetchWeather(city);
     input.value = '';
 });
 
 async function fetchWeather(city) {
-    loading.classList.remove('hidden');
+    loading.classList.add('hidden');
     error.classList.add('hidden');
     weather.classList.add('hidden');
-try {
-    const url = `${API_URL}?q=${city}&units=metric&appid=${API_KEY}`;
-    
-    const response = await fetch(url);
-
-    if (!response.ok) {
-        throw new Error(
-            response.status === 404
-            ? 'City not found'
-            : `Error: ${response.status}`
-        );                   
+    try{
+        const url = `${API_URL}?q=${city}&units=metric&appid=${API_KEY}`;
+        const response = await fetch(url);
+        if(!response.ok) {
+            throw new Error(
+                response.status === 404
+                ? 'City not found'
+                : `Error: ${response.status}`
+            );
+        }
+        const data = await response.json();
+        displayWeather(data);
+    } catch (err) {
+        loading.classList.add('hidden');
+        error.textContent = err.message;
+        error.classList.remove('hidden');
     }
-
-    const data = await response.json();
-
-    displayWeather(data);
-
-}catch (err) {
-    error.textContent = err.message;
-    error.classList.remove('hidden');
-
-} finally {
-    loadfing.classList.add('hidden');
-}
 }
 
 function displayWeather(data) {
@@ -59,9 +51,9 @@ function displayWeather(data) {
     data.weather[0].description.slice(1);
 
     document.getElementById('details').textContent =
-    `Feels like ${Math.round(data.main.feels_like)}°C .` +
-    `Humidity ${data.main.humidity}% .` +
+    `Feels like ${Math.round(data.main.feels_like)}°C . ` +
+    `Humidity ${data.main.humidity}% . ` +
     `Wind ${Math.round(data.wind.speed)} m/s`;
 
-    weather.classList.remove('hidden')
+    weather.classList.remove('hidden');
 }
